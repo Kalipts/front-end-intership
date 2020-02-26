5161864f975815440977a9689541e628a68907db
Lộc Phạm Xuân <halfbloodprincess9@gmail.com>
2020-02-26 04:14:07 +0000

    Display booking with api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Header from './components/Header';
-import DashboardPage from './containers/DashboardPage';
+import App from "./App";
 
-function Greeting() {
-  const abc = new Array(200000).fill(1);
-  const abcRender = abc.map((item,index)=>(<div key={index}>{item}</div>))
-  return <h1>{abcRender}</h1>;
-}
-
-ReactDOM.render(<DashboardPage />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
